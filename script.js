a7de73f8febe831bb583b8fb6b982aa6790902fa
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-23 06:20:20 +0100

    Forteenth Commit: Implement createList() function

---
+++ script.js
@@ -0,0 +1,47 @@
+const draggable_list = document.getElementById("draggable-list");
+const check = document.getElementById("check");
+
+const richestPeople = [
+  "Jeff Bezos",
+  "Elon Musk",
+  "Bernard Arnault",
+  "Bill Gates",
+  "Mark Zuckerberg",
+  "Warren Buffett",
+  "Larry Ellison",
+  "Larry Page",
+  "Sergey Brin",
+  "Mukesh Ambani",
+];
+
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+function createList() {
+  [...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
+
+      listItem.setAttribute("data-index", index);
+
+      listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+         <div class="draggable" draggable = "true">
+            <p class="person-name">${person}</p>
+            <i class="fas fa-grip-lines"></i>
+        </div>     
+        `;
+
+      listItems.push(listItem);
+
+      draggable_list.appendChild(listItem);
+    });
+
+  addEventListener();
+}
